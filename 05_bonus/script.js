1d490d89179900dd7fab1245aca8479de5311ce2
HikariNM <nicole.hikari.mascellino@gmail.com>
2025-11-08 10:57:50 +0100

    svolto quinto esercizio bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,10 +9,24 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function greetingsByTime(name){
+    const hours = new Date().getHours();
+    const minutes = new Date().getMinutes()
+    console.log(`Sono le ${hours}:${minutes}`);
+    if (hours > 5 && hours <= 13 ) {
+        return `Buongiorno ${name}`;
+    } else if (hours > 13 && hours <= 17){
+        return `Buon pomeriggio ${name}`
+    } else if (hours > 17 && hours <= 21){
+        return `Buonasera ${name}`;
+    } else {
+        return `Buonanotte ${name}`;
+    }
+    
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(greetingsByTime(name))
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
